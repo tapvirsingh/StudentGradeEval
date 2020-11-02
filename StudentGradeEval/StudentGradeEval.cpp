5e1ad929cce59ef698bf1f7cda498939d9b813d4
Tapvir Singh <tapvir@gmail.com>
2020-11-02 15:50:49 +0530

    Working state of the app

--- StudentGradeEval/StudentGradeEval.cpp
+++ StudentGradeEval/StudentGradeEval.cpp
@@ -2,6 +2,19 @@
 //
 
 #include <iostream>
+#include <conio.h>
+
+// As getch() doesn't work
+// in Visual Studio but _getch() does, following
+// lines of code defines it.
+
+#ifndef getch()
+
+#define getch() _getch()
+
+#endif // !getch()
+
+
 using namespace std;
 
 class GradeEvaluator {
@@ -9,24 +22,77 @@ class GradeEvaluator {
 protected:
 
     unsigned int marks;
+    float grade;
+    string classifedReport;
 
 public:
 
     // Constructor that takes the marks of the student.s
     GradeEvaluator(unsigned int _marks) {
+        // Assigning values.
         marks = _marks;
+        grade = 0;
+        classifedReport = "";
     }
 
 
-    //Evaluate
+    //Evaluate the grade
     float evaluate() {
-            
+
+        if (marks >= 98 && marks <= 100) 
+        {
+            grade = 1;
+        }
+        else if(marks >= 95 && marks <= 97)
+        {
+            grade = 1.25;
+        }
+        else if (marks >= 92 && marks <= 94)
+        {   
+            grade = 1.5;
+        }
+        else if (marks >= 89 && marks <= 91)
+        {
+            grade = 1.75;
+        }
+        else if (marks >= 86 && marks <= 88)
+        {
+            grade = 2;
+        }
+        else if (marks >= 83 && marks <= 85)
+        {
+            grade = 2.25;
+        }
+        else if (marks >= 80 && marks <= 82)
+        {
+            grade = 2.5;
+        }
+        else if (marks >= 77 && marks <= 79)
+        {
+            grade = 2.75;
+        }   
+        else if (marks == 75 || marks == 76)
+        {
+            grade = 3;
+        }
+        else if (marks <= 74)
+        {
+            grade = 5;
+        }
+
+        return grade;
     }
 
 
     // Get the classified report regarding Passed / Failed.
     string classifyReport() {
+        // If the student's grade is less than 5 then he has passed.
+        if (grade < 5) {
+            return "passed";
+        }
 
+        // else he has failed.
+       return "failed";
     }
 
 
@@ -52,6 +118,11 @@ int main()
 
     // Throw the output with result.
     cout << "The final grade of the student is " << finalGrade << " , the student has " << report << endl;
+
+    // Display the output and wait for the user to press any key to exit.
+    cout << "Press any key to exit.";
+
+    getch();
 
     // All ok
     return 0;
